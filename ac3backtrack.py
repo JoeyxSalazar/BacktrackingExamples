e99409e275e8dd19561c510dd3c19f42f517e535
JoeyxSalazar <salazarxjoey@gmail.com>
2023-10-03 15:47:35 -0400

    Hopefully all good now, need times

---
+++ ac3backtrack.py
@@ -0,0 +1,52 @@
+import random
+
+class ac3bt:
+    def __init__(self, ind):
+        self.i = ind
+        
+    
+    def ac3(self):
+        # Initialize a queue with all constraints
+        queue = [(state_A, state_B) for state_A in self.i.map.states for state_B in self.i.map.neighbors[state_A]]
+        while queue:
+            state_X, state_Y = queue.pop(0)
+            if self.revise(state_X, state_Y):
+                if len(self.i.domains[state_X]) == 0:
+                    return False  # Inconsistent assignment
+                for neighbor in self.i.map.neighbors[state_X]:
+                    if neighbor != state_Y:
+                        queue.append((neighbor, state_X))
+        return True  # AC-3 succeeded; the domains are consistent
+
+    def revise(self, state_X, state_Y):
+        revised = False
+        for color_X in self.i.domains[state_X][:]:
+            if not any(self.i.isColorValid(state_Y, color_Y) for color_Y in self.i.domains[state_Y]):
+                self.i.domains[state_X].remove(color_X)
+                revised = True
+        return revised
+
+    def backtrack(self):
+        if self.i.isGoal() and all(self.i.genome[key] != 0 for key in self.i.genome.keys()):
+            print('Goal')
+            return True
+
+        state = self.i.selectUnassignedState()
+        if state is None:
+                return True # All states are assigned colors
+
+        for color in self.i.domains[state]:
+            if self.i.isColorValid(state, color):
+                prev_color = self.i.genome[state]
+                self.i.genome[state] = color
+                self.i.updateDomains()
+                self.i.updateFitness()
+
+                if self.ac3():  # Apply AC-3 for constraint propagation
+                    if self.backtrack():  # Recursively backtrack
+                        return True
+
+                self.i.genome[state] = prev_color  # Backtrack
+                self.i.updateDomains()
+
+        return False
---
+++ mincon.py
@@ -0,0 +1,66 @@
+import random 
+
+class mc:
+    def __init__(self, ind):
+        self.i = ind
+        pass
+
+    def min_conflicts(self, max_iterations):
+      for j in range(max_iterations):
+          conflicts = self.get_conflicts()
+          if conflicts == 0:
+              return True  # Solution found
+          variable = self.select_variable_with_conflicts(conflicts)
+          value = self.select_value_minimizing_conflicts(variable)
+          self.i.genome[variable] = value
+          self.i.updateFitness()
+          if j % 200 == 0: #Exit local minima
+              self.i.mutate()
+      return False  # No solution found within the max_iterations
+
+    def get_conflicts(self):
+        conflicts = 0
+        for border in self.i.map.borders:
+            s1 = self.i.genome[self.i.map.states[border.index1]]
+            s2 = self.i.genome[self.i.map.states[border.index2]]
+            if s1 == s2:
+                conflicts += 1
+        return conflicts
+    
+    def has_conflicts(self, state):
+        state_idx = self.i.map.states.index(state)
+        for border in self.i.map.borders:
+            if state_idx == border.index1 or state_idx == border.index2:
+                s1 = self.i.genome[self.i.map.states[border.index1]]
+                s2 = self.i.genome[self.i.map.states[border.index2]]
+                if s1 == s2:
+                    return True
+        return False
+
+    def select_variable_with_conflicts(self, conflicts):
+        # Select a variable (region) with conflicts randomly.
+        conflicted_variables = [state for state in self.i.map.states if self.has_conflicts(state)]
+        return random.choice(conflicted_variables)
+
+    def select_value_minimizing_conflicts(self, variable):
+        current_color = self.i.genome[variable]
+        neighbor_colors = set(self.i.genome[state] for state in self.i.map.neighbors[variable])
+        #neighbor_colors.discard(current_color)
+
+        # Initialize variables to track the best color and its conflicts
+        best_color = current_color
+        best_conflicts = self.get_conflicts()
+
+        for color in neighbor_colors:
+            self.i.genome[variable] = color
+            conflicts = self.get_conflicts()
+
+            # If this color reduces conflicts, update the best_color and best_conflicts
+            if conflicts < best_conflicts:
+                best_color = color
+                best_conflicts = conflicts
+
+        # Restore the original color.
+        self.i.genome[variable] = current_color
+        return best_color
+ 
